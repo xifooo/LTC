dc34c87443dd8377d5ea72ed8d8d0c15cc85cec6
xifooo <xifooo@gmail.com>
2023-02-16 00:16:42 +0800

    some small changes

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
-def A():
-    def B():
-        print("hello")
-    return B()
+class A:
+    def __format__(self, spec):
+        if spec == "x":
+            return "0XA"
+        return "<A>"
+    
+# print(f"{A()}")
+# print(f"{A():x}")
 
-# A()
-result = A()
-# print(A())
-print(result)
+print("{}".format(15))
+print("{:b}".format(15))
+print("{:x}".format(15))
+
+print(f"{15}")
+print(f"{15:b}")
+print(f"{15:x}")
